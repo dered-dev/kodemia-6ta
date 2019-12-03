c7b76b893f168d01e874e60d605733885e94b104
dered-dev <jlcc170@gmail.com>
2019-12-02 22:10:26 -0600

    Ejercicio 1: Divisas

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 console.log("Hola Koders")
 
+// Ejercicio 1 Divisas
+
+var valueMXNToday = 1; // to convert
+var valueUSDToday = 19.57; // value MXN to USD today
+var valueEURToday = 21.68; // value MXN to EUR today
+var valueMXNtoCUPToday = 0.051; // value MXN to CUP today
+var valueCucToday = 265000; // value to 
+
+var valueCUCToday = valueMXNtoCUPToday * valueCucToday ;
+
+console.log('Dollars: '+ valueMXNToday / valueUSDToday );
+console.log('Euro: '+ valueMXNToday / valueEURToday);
+console.log('Cuban Convertible Peso: '+ valueMXNToday / valueCucToday);
+
+
