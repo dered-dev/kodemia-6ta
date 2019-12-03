0fb48e75c371e7acc23b097e0869f7058e23705e
dered-dev <jlcc170@gmail.com>
2019-12-03 12:13:14 -0600

    UPDATE Final version

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 console.log("Hola Koders")
-
 // Ejercicio 1 Divisas
 
-var valueMXNToday = 1; // to convert
-var valueUSDToday = 19.57; // value MXN to USD today
-var valueEURToday = 21.68; // value MXN to EUR today
-var valueMXNtoCUPToday = 0.051; // value MXN to CUP today
-var valueCucToday = 265000; // value to 
-
-var valueCUCToday = valueMXNtoCUPToday * valueCucToday ;
-
-console.log('Dollars: '+ valueMXNToday / valueUSDToday );
-console.log('Euro: '+ valueMXNToday / valueEURToday);
-console.log('Cuban Convertible Peso: '+ valueMXNToday / valueCucToday);
-
+var MXNToConvert = 1;
+var valueUSDToday = 19.57;
+var valueEURToday = 21.68;
+var valueMXNtoCUPToday = 0.76;
+var valueCUCtoCUPToday = 25.75;
+var CUCToCUP = valueMXNtoCUPToday * valueCUCtoCUPToday;
+console.log('Dollars: '+ MXNToConvert / valueUSDToday );
+console.log('Euro: '+ MXNToConvert / valueEURToday);
+console.log('Cuban Convertible Peso: '+ MXNToConvert / CUCToCUP);
 
