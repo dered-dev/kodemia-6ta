efabf0c7f5c2916fdb0b6b2a9a233b192a575046
dered-dev <jlcc170@gmail.com>
2019-12-03 20:48:19 -0600

    UPDATE compare number options

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ var singularWord = pluralWord[0] + pluralWord[1] + pluralWord[2] + pluralWord[3]
 console.log(singularWord)
 
 
-
-
-
+// compare number
 var numberCompare = 11;
 if((numberCompare >= 0 && numberCompare <= 100) && (numberCompare%10) === 0 ) {
-     console.log('Esta dentro del rango 0 y 100 y es multiplo de 10')
+     console.log('Esta dentro del rango 0 y 100 y es múltiplo de 10')
+}else if(numberCompare%10 === 0){
+    console.warn('Solo es multiplo de 10');
+}else if(numberCompare>=0 && (numberCompare%10) !== 0){
+    console.warn('Es positivo pero no es multiplo de 10');
 }else{
-    console.warn('No se encuentra en el rango de 0 y 100 y no es múltiplo de 10')
+    console.error('No se encuentra en el rango de 0 y 100 y no es múltiplo de 10')
 }
+
