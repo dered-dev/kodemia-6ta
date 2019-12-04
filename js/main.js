f5dab83ab578b2518dbd8d61db34feae318e047e
dered-dev <jlcc170@gmail.com>
2019-12-03 22:25:17 -0600

    UPDATE exercise length

--- js/main.js
+++ js/main.js
@@ -16,3 +16,38 @@ if((numberCompare >= 0 && numberCompare <= 100) && (numberCompare%10) === 0 ) {
     console.error('No se encuentra en el rango de 0 y 100 y no es múltiplo de 10')
 }
 
+// TEMPLATE LITERALS string interpolation
+console.log(`El resultado de 1 + 10 es: ${1+10} y otra suma ${1+1}`)
+
+// STRING methods
+var str = "perro"
+console.log(str.length)
+console.log(str.charAt(position))
+console.log(str.concat(str1,str2))
+console.log(str.includes('str1',position))
+console.log(str.slice(7, 13));
+
+// Exercise
+// Determinar si la longitud de la palabra ingresa es par
+// Verificar que esa palabra tenga una longitud entre 0 y 10
+// Verificar que esa palabra tenga una longitud entre 10 y 15
+
+var strCompare  = "perro"
+if(strCompare.length % 2 === 0){
+    console.log('El numero es par')
+    if(strCompare.length >= 0){
+        if(strCompare.length <= 10){
+            console.log('El numero es mayor que 0 y menor que 10')
+        }else if(strCompare.length <= 15){
+            console.log('El numero es mayor o igual a 11 y menor que 15')
+        }else{
+            console.log('El numero es mayor que 15')
+        }
+    }
+}else{
+    console.warn('No es un numero par')
+}
+
+
+
+
