38ee9449ba9333478a1c004e6a72aae483d59ee5
dered-dev <jlcc170@gmail.com>
2019-12-03 19:53:24 -0600

    CREATE singular word

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
-console.log("Hola Koders")
 
+
+var pluralWord = 'perros';
+var singularWord = pluralWord[0] + pluralWord[1] + pluralWord[2] + pluralWord[3] + pluralWord[4];
+console.log(singularWord)
