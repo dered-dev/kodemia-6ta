d8db625ed6ebadf44d23104338371d5c5e42aa4a
dered-dev <jlcc170@gmail.com>
2019-12-05 22:05:07 -0600

    ADD functions

--- js/main.js
+++ js/main.js
@@ -1,2 +1,104 @@
-console.log("Hola Koders")
+/*
+    Exercise 1.
+    Se tiene una lista con el nombre de cada uno de los empleados, con los días que faltaron al trabajo
+    Ej -> [ [David, 4], [Jose, 2] ]
+    Si el sueldo diario son $450.00, y por cada día que faltaron se le descuenta el 2% del salario ( acumulable),
+    cuanto es el sueldo final del empleado.
+    OUTPUT: {name}: ${salary}.00
+
+*/
+
+var empleados        = [ ['Jose',4],['Pedro',2],['Luis',3]];
+var salaryTotal      = [];
+var payDays          = 30;
+var salarioDay       = 450;
+var percentDisccount = 2;
+
+// salarios 
+for(var count = 0; count <= empleados.length-1; count++){
+    var salaryEmployee = salarioDay * payDays;
+    for(var countInner = 0; countInner <= empleados[count][1]; countInner++){
+        salaryEmployee = (salaryEmployee * (1-percentDisccount/100))
+        console.log(salaryEmployee)
+    }
+    salaryTotal[count] = salaryEmployee;
+}
+
+// print salary final
+console.log(salaryTotal)
+
+
+
+/*
+Functions:
+*/
+const PI = 3.1416
+function circleArea(radius){
+    return PI*(radius**2)
+}
+
+console.log(circleArea(2))
+console.log(circleArea(4))
+console.log(circleArea(8))  
+
+
+/*
+Exercise 2.
+Input: 
+*/
+
+
+var names = [['jorge',''],['','Luna'],['','perez'],['',''],['',''],['',''],['',''],['','']]
+function concatNames(arrayNames){
+    for(var count = 0; count <= arrayNames.length - 1; count++){
+        var name = arrayNames[count][0];
+        var lastName = arrayNames[count][1];
+        if(name === "" && lastName === ""){
+            // both
+            console.log(`Name ${count + 1}: --- ---`)
+
+        }else if(name === "" && lastName !== ""){
+            // one
+            console.log(`Name ${count + 1}: --- ${ lastName } `)
+            
+        }else if(name !== "" && lastName === ""){
+            // two
+            console.log(`Name ${count + 1}: ${ name } ---`)
+
+        }else{
+            // any
+            console.log(`Name ${count + 1}: ${ name } ${ lastName } `)
+        }
+    }
+}
+concatNames(names)
+
+// function verify a palindrome
+function verifyPalindrome(frase){
+    var newFrase = frase.replace(/\s+/g, '').toLowerCase();
+    var reverse = ""
+    for(var count = newFrase.length -1; count >= 0; count--){
+        reverse += newFrase[count];
+    }
+    if(newFrase === reverse){
+        return true;
+    }else{
+        return false;
+    }
+}
+verifyPalindrome('anita lava la tina')
+
+// resolver algoritmo
+// n1=1 n2=5 n3=13 n4=35 shape area
+
+// n1 = 1
+// n2 = 5
+// n3 = 13 
+// n4 = 25
+
+function shape(n){
+    
+}
+
+
 
