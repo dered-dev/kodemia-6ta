a4ace3998df35f36a28da91bf50390ca1fafb77a
dered-dev <jlcc170@gmail.com>
2019-12-10 22:11:39 -0600

    Objects

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
-console.log("Hola Koders")
+/**
+ * Exercise 1: Dado un array de string
+ * Retornar un string que contenga un array que contenga todas las iniciales
+ */
 
+var wordsArray = ['hola','perro','Alan','oso','Arbol']
+
+// solution with .map()
+const singleWordMap    = arr => arr.map(word => word.charAt(0)).join('')
+console.log(singleWordMap(wordsArray))
+
+// solution with .map()
+const singleWordReduce = arrToReduce => arrToReduce.reduce((accumulate ,current) => accumulate + current.charAt(0),'')
+console.log(singleWordReduce(wordsArray))
+
+
+/**
+ * Objects
+ */
+
+var dog = {
+    name : "algo",
+    edad : 10,
+    weight: {
+        unit: 'kg',
+        ammount: 10
+    }
+}
