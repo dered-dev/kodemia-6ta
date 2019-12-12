6acbc7f8a701c05c4455ed5989939c3d5a1b27a8
dered-dev <jlcc170@gmail.com>
2019-12-12 11:35:51 -0600

    CREATE function getListRFC

--- js/main.js
+++ js/main.js
@@ -85,3 +85,57 @@ const getFinalAverage = group => {
 console.log(averageStudents(arrayStudents))
 console.log(getFinalAverage(arrayStudents))
 
+/**
+ * Exerccise 2:
+ * Abstraer el objeto persona con los datos necesarios para sacar el RFC sin homoclave, 
+ * crear un array de personas y obtener un array con el RFC de cada uno
+ */
+
+
+var listRFCs = [
+    {
+        apaterno : 'camarillo',
+        amaterno : 'cristobal',
+        nombres  : 'jorge luis',
+        year : '1991',
+        month : '05',
+        day : '12',
+        createRFC: function(){
+            return getRFC(this.apaterno,this.amaterno,this.nombres,this.year,this.month,this.day)
+        }
+    },
+    {
+        apaterno : 'camarillo',
+        amaterno : 'luna',
+        nombres  : 'mario',
+        year : '1995',
+        month : '05',
+        day : '12',
+        createRFC: function(){
+            return getRFC(this.apaterno,this.amaterno,this.nombres,this.year,this.month,this.day)
+        }
+    }
+]
+
+const getRFC = (aPaterno, aMaterno, nombres,year,month, day, temp = "") => {
+    var vowels = 'aeiou'
+    var firstLetter = aPaterno.split('').reduce(function(acc,letter,index){
+        if(vowels.includes(letter) && index > 1){
+            temp += letter;
+        }
+        return temp
+    }).substring(0,1);
+    var letters = `${ aPaterno.substring(0,1) }${ firstLetter }${ aMaterno.substring(0,1) }${ nombres[0] }`;
+    var numbers = `${ year.substring(2) }${ month.substring(0,2) }${ day.substring(0,2) }`
+    return letters + numbers
+}
+const getListRFC = arr => {
+    return arr.map((cvalue) => cvalue.createRFC())
+}
+console.log(getListRFC(listRFCs))
+
+/**
+ * Exercise 3
+ * Abstraer el objeto alumno, y dado un array de alumnos dar promedio por materia por periodo
+ */
+
