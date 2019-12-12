6038fe8532502d58f75ff57ffbc6fafc1e1a5944
dered-dev <jlcc170@gmail.com>
2019-12-11 19:41:44 -0600

    CREATE function capitalizePhrase()

--- js/main.js
+++ js/main.js
@@ -1,2 +1,32 @@
-console.log("Hola Koders")
+/**
+ * Exercise 1:
+ *  Dado un frase, Capitalizar toda la frase
+ *  'hOlA A todOs' => 'Hola A Todos'
+ */
 
+function capitalizePhrase(phrase){
+    var arrToCapitalize = phrase.toLowerCase().split(' ');
+    var arrCapitalized  = []
+    for(var count = 0; count < arrToCapitalize.length; count++){
+        var word = arrToCapitalize[count];
+        arrCapitalized[count] = `${word[0].toUpperCase()}${word.slice(1)}`
+    }
+    return arrCapitalized.join(' ');
+}
+var phraseToCapitalize = 'hOlA A todOs'
+console.log(capitalizePhrase(phraseToCapitalize))
+
+
+
+/**
+ * Exercise 2:
+ *  Dado un array múltiple, contar la cantidad de 1 en el array
+ *  [[1,2,0], ['a',3,0], [1,0,1]]; => output: 3
+ */
+
+
+
+/**
+ * Exercise 3:
+ * Dada un frase, determinar cual es la palabra mas larga en ella
+ */
