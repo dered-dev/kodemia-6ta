624b94841d4ccbf530a52219d533096d77f9c508
dered-dev <jlcc170@gmail.com>
2019-12-13 01:34:43 -0600

    ADD challenge 2, objectToArray function

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
  */
 
 var newArray = [
-  ['jorge','perez',25],
-  ['juan','torres',24],
-  ['daniel','guzman',23],
+  ['jorge', 'perez', 25],
+  ['juan', 'torres', 24],
+  ['daniel', 'guzman', 23],
 ]
 
-function Student(firstName,lastName,age){
+function Student(firstName, lastName, age) {
   this.firstName = firstName
   this.lastName = lastName
   this.age = age
@@ -32,12 +32,28 @@ console.log(arrayMaped(newArray))
  * @param {*} len 
  */
 
-const arrayMultiple = (mul,len) => {
-  var newArr = []
-  for(var count = 1; count <= len; count++){
-    newArr.push(mul*count)
+const arrayMultiple = (mul, len) => {
+  let newArr = []
+  for (let count = 1; count <= len; count++) {
+    newArr.push(mul * count)
   }
   return newArr
 }
 
-console.log(arrayMultiple(17,6))
+console.log(arrayMultiple(17, 6))
+
+/**
+ * Challenge 2: escriba una funcion, que convierta un objeto en una matriz,
+ * donde cada elemento representa un par clave valor
+ */
+
+const objectToArray = obj => {
+  let newArr = [];
+  for (let key in obj) {
+    newArr.push([key,obj[key]])
+  }
+  return newArr
+}
+
+var objToConvert = { a: 1, b: 2 };
+console.log(objectToArray(objToConvert))
